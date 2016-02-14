cf4ed5b011ff687c1f43f0a6ec91b2d8fbd1a1fc
wangtjwork <wangpanda1993@tamu.edu>
2016-02-13 19:27:03 -0600

    Update wang_tianjian_4.rb

--- HW2/wang_tianjian_4.rb
+++ HW2/wang_tianjian_4.rb
@@ -1,11 +1,7 @@
-class Array
-    def palindrome?
-        self == self.reverse
-    end
-end
-
-class Hash
-    def palindrome?
-        true
-    end
+module Enumerable
+  def palindrome?
+    self == self.reverse
+  rescue NoMethodError
+    self.to_a == self.to_a.reverse
+  end
 end
